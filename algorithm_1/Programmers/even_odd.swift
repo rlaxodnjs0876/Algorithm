b67ab1b63b50223d4f342bfb5dde994e9041a160
Kim <xodnjs0876@gmail.com>
2022-04-19 11:50:02 +0900

    [ADD] Even_Odd

---
+++ algorithm_1/Programmers/even_odd.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  algorithm_1
+//
+//  Created by 김태원 on 2022/04/18.
+//
+
+import Foundation
+
+func solution(_ num:Int) -> String {
+    
+    if num%2 == 0 {
+        return "Even"
+    }
+    else {
+        return "Odd"
+    }
+}
+
