abcb849576ce60ee374118a89d117f6ac2f14400
Kim <xodnjs0876@gmail.com>
2022-04-21 18:49:45 +0900

    [ADD] CT 12919

---
+++ algorithm_1/Programmers/CT 12919.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  algorithm_1
+//
+//  Created by 김태원 on 2022/04/21.
+//
+
+import Foundation
+
+func solution(_ seoul:[String]) -> String {
+    
+    return "김서방은 \(seoul.firstIndex(of: "Kim")!)에 있다"
+    
+}
