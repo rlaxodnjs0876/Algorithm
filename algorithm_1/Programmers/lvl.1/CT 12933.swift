42589b4fe80aa8b7b4736fddd0085fd4f250dcd7
Kim <xodnjs0876@gmail.com>
2022-04-29 15:18:44 +0900

    [ADD] CT 12933

---
+++ algorithm_1/Programmers/lvl.1/CT 12933.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  algorithm_1
+//
+//  Created by 김태원 on 2022/04/25.
+//
+
+import Foundation
+
+func solution(_ n:Int64) -> Int64 {
+    return Int64(String(String(n).sorted(by: >)))!
+}
+
