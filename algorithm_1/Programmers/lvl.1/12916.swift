5d07087dd1fdd60f6463ff50174905b0154adf83
Kim <xodnjs0876@gmail.com>
2022-07-22 13:44:36 +0900

    [ADD] 문자열 내 p와 y의 개수

---
+++ algorithm_1/Programmers/lvl.1/12916.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  algorithm_1
+//
+//  Created by 김태원 on 2022/07/19.
+//
+// 문자열 내 p와 y의 개수
+
+//import Foundation
+//
+//func solution(_ s:String) -> Bool
+//{
+//    let a = s.lowercased()
+//    let p = a.filter { $0=="p"}.count
+//    let y = a.filter { $0=="y"}.count
+//    
+//    return p==y ? true : false
+//}
+//
+//
