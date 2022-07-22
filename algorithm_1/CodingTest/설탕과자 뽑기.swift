bed931084f8b49763ef44071add2141353d144e7
Kim <xodnjs0876@gmail.com>
2022-07-22 13:44:01 +0900

    [ADD] 설탕과자뽑기

---
+++ algorithm_1/CodingTest/설탕과자 뽑기.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  algorithm_1
+//
+//  Created by 김태원 on 2022/07/20.
+//
+
+//w h n l d xy
+//import Foundation
+//
+//var arr = Array(repeating: Array(repeating: 0, count: 101), count: 101)
+//
+//let a = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+//
+//let (w,h) = (a[0],a[1])
+//
+//let n = Int(readLine()!)!
+//
+//for _ in 0..<n{
+//    let b = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+//    let (l,d,x,y) = (b[0],b[1],b[2],b[3])
+//    for j in 0..<l{
+//        if(d == 1){ arr[x+j][y] = 1 }
+//        else{ arr[x][y+j] = 1}
+//    }
+//}
+//
+//for i in 1...w{
+//    for j in 1...h{
+//        print(arr[i][j], separator: " ", terminator: " ")
+//    }
+//    print("")
+//}
+
+
