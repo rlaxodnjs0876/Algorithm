f34dfd158a317af23f22c3092af856d4dec497c6
Kim <xodnjs0876@gmail.com>
2022-07-22 13:45:09 +0900

    [ADD] 문자열 다루기 기본

---
+++ algorithm_1/Programmers/lvl.1/CT 12918.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  algorithm_1
+//
+//  Created by 김태원 on 2022/07/14.
+//
+
+////12918
+//import Foundation
+//
+//func solution(_ s:String) -> Bool {
+//    let c = s.count
+//    if c == 4 || c == 6{
+//        return Int(s) != nil
+//    }
+//    return false
+//}
+//
