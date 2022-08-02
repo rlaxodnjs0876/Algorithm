46a902f01d9d6115b0e89b3f690cd66ebe7db3b2
Kim <xodnjs0876@gmail.com>
2022-08-02 21:02:54 +0900

    [ADD] DP 예제

---
+++ algorithm_1/CodingTest/DP 예제.swift
@@ -0,0 +1,37 @@
+//
+//  main.swift
+//  algorithm_1
+//
+//  Created by 김태원 on 2022/07/21.
+//
+
+import Foundation
+
+//func fibo_td(_ n:Int)->Int{
+//    var d = [Int](repeating: 0, count: 101)
+//    if n==1 || n==2{
+//        return 1
+//    }
+//
+//        if (d[n] == 1){ return d[n] }
+//
+//        d[n] = fibo_td(n-1) + fibo_td(n-2)
+//
+//        return d[n]
+//}
+
+//func fibo_bu(_ n:Int) -> Int{
+//    var d = [Int](repeating: 0, count: 101)
+//    d[1]=1
+//    d[2]=1
+//
+//    for i in 3...n{
+//        d[i] = d[i-1] + d[i-2]
+//    }
+//
+//    return d[n]
+//}
+
+
+
+
