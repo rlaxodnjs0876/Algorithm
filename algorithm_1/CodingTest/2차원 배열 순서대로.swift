57e92b5f4d41363fc9ab7cb2319cd1e9a400136d
Kim <xodnjs0876@gmail.com>
2022-08-02 21:03:16 +0900

    [ADD] 2차원 배열

---
+++ algorithm_1/CodingTest/2차원 배열 순서대로.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  algorithm_1
+//
+//  Created by 김태원 on 2022/07/20.
+//
+
+//import Foundation
+//
+//var arr = Array(repeating: Array(repeating: 0, count: 101), count: 101)
+//
+//let a = Int(readLine()!)!
+//let b = Int(readLine()!)!
+//var count = 1
+//
+//for i in 0..<a{
+//    for j in 0..<b{
+//        arr[i][j] = count
+//        count+=1
+//        print(arr[i][j],separator: " ", terminator: " ")
+//    }
+//    print(" ")
+//}
+
+
+    
+
