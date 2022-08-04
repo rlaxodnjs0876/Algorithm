d2dcee6588ea2989a704f63efb8baeebf4068c74
Kim <xodnjs0876@gmail.com>
2022-08-04 21:06:31 +0900

    [ADD] 피보나치

---
+++ algorithm_1/Programmers/lvl.2/CT 12945.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  algorithm_1
+//
+//  Created by 김태원 on 2022/06/20.
+//
+
+//import Foundation
+//
+//func solution(_ n:Int) -> Int {
+//    var fifo = [0,1]
+//    
+//    for i in 2...n{
+//        return fifo.append(Int(fifo[i-1] + fifo[i-2])) % 1234567
+//    }
+//}
